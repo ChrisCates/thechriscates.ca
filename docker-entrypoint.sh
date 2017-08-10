b634e1edf8f1eb7e33b77b580898757e0b817044
Chris Cates <hello@chriscates.ca>
2017-08-10 00:32:19 -0400

    Updated command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 #uncomment this if you use ecto
 #mix do ecto.create, ecto.migrate
 
-elixir -pa _build/prod/consilidated -S mix phoenix.server
+elixir -pa _build/prod/consilidated -S mix phx.server
 
 exec $@
